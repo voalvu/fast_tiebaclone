01cbf67a62affa582d36cc8788aa73453cc0dbad
voalvu <voalvu@gmail.com>
2025-03-14 22:29:45 +0100

    verceljson build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Download and build libmicrohttpd
-wget https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-latest.tar.gz
+set -e  # Exit on any error
+
+# Download and build libmicrohttpd using curl
+
+curl -LO https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-latest.tar.gz
+
 tar -xzf libmicrohttpd-latest.tar.gz
-cd libmicrohttpd-* || exit
+
+cd "$(ls -d libmicrohttpd-*/ | head -n 1)"
+
 ./configure --prefix="$PWD/../libmicrohttpd" --enable-static
+
 make && make install
+
 cd ..
 
-# Compile your C code
-gcc -o tieba tieba.c \
-  -I./libmicrohttpd/include \
-  -L./libmicrohttpd/lib \
-  -lmicrohttpd -lws2_32
+# Compile the C code
+
+gcc -o tieba tieba.c -I./libmicrohttpd/include -L./libmicrohttpd/lib -lmicrohttpd -lws2_32
